d0861d972f300b53243d3d673e3b72351b8d4b71
Irfannudien <muhammadirfannudien@gmail.com>
2021-11-16 22:02:26 +0700

    update panel landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,59 +15,57 @@ function Navbar() {
   };
   return (
     <>
-      <div className={styles.navbar}>
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <div className="container-fluid container">
-            <Link href="/">
-              <a className={styles.navbarBrand}>Mendoan.id</a>
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav ms-auto">
-                <Link href="/login">
-                  <div className={styles.navLink}>
-                    <a
-                      className={"nav-link" + isActive("/login")}
-                      aria-current="page"
-                    >
-                      <i className="far fa-user" aria-hidden="true"></i> Login
-                    </a>
-                  </div>
-                </Link>
-                <Link href="/sign-up">
-                  <div className={styles.navLink}>
-                    <a
-                      className={"nav-link" + isActive("/sign-up")}
-                      aria-current="page"
-                    >
-                      <i className="far fa-user" aria-hidden="true"></i> Sign Up
-                    </a>
-                  </div>
-                </Link>
-                <Link href="/cart">
-                  <div className={styles.cart}>
-                    <i className="bi bi-bag-check" aria-hidden="true"></i>
-                    <a
-                      className={"nav-link" + isActive("/cart")}
-                      aria-current="page"
-                    ></a>
-                  </div>
-                </Link>
-              </div>
+      <nav className={`navbar navbar-expand-lg navbar-light ${styles.navbar}`}>
+        <div className="container-fluid container">
+          <Link href="/">
+            <a className={styles.navbarBrand}>Mendoan.id</a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <Link href="/login">
+                <div className={styles.navLink}>
+                  <a
+                    className={"nav-link" + isActive("/login")}
+                    aria-current="page"
+                  >
+                    <i className="far fa-user" aria-hidden="true"></i> Login
+                  </a>
+                </div>
+              </Link>
+              <Link href="/sign-up">
+                <div className={styles.navLink}>
+                  <a
+                    className={"nav-link" + isActive("/sign-up")}
+                    aria-current="page"
+                  >
+                    <i className="far fa-user" aria-hidden="true"></i> Sign Up
+                  </a>
+                </div>
+              </Link>
+              <Link href="/cart">
+                <div className={styles.cart}>
+                  <i className="bi bi-bag-check" aria-hidden="true"></i>
+                  <a
+                    className={"nav-link" + isActive("/cart")}
+                    aria-current="page"
+                  ></a>
+                </div>
+              </Link>
             </div>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,65 @@ const Home = () => {
     <>
       {/* ====== TEXTBOX ========= */}
       <div className={styles.textBox}>
-        <div className="container">
-          <div className={styles.intro}>
-            <h1 className={styles.display4}>
-              Mau ada tamu datang atau ga, pesan gorengan di{" "}
-              <span>Mendoan</span> aja!
-            </h1>
-            <p>
-              Cobain deh, gorengan paling hits di <span>Indonesia</span>{" "}
-              sekarang juga dari rumah kamu!
-            </p>
-            <div className={styles.tombol}>
-              <a
-                href="/login"
-                type="button"
-                className="btn btn-warning"
-                style={{ borderRadius: "10px" }}
-              >
-                <h6>Pesan Sekarang</h6>
-              </a>
-            </div>
+        <div className={`container ${styles.intro}`}>
+          <h1 className={styles.display4}>
+            Mau ada tamu datang atau ga, pesan gorengan di <span>Mendoan</span>{" "}
+            aja!
+          </h1>
+          <p>
+            Cobain deh, gorengan paling hits di <span>Indonesia</span> sekarang
+            juga dari rumah kamu!
+          </p>
+          <div className={styles.tombol}>
+            <a
+              href="/login"
+              type="button"
+              className="btn btn-warning"
+              style={{ borderRadius: "10px" }}
+            >
+              <h6>Pesan Sekarang</h6>
+            </a>
           </div>
         </div>
       </div>
-      {/* =====CONTENT===== */}
+
+      {/* ===== CONTENT ===== */}
       <div className="container">
-        <h2>CONTENT</h2>
+        {/* ===== PANEL ===== */}
+        <div className="row justify-content-center" style={{ height: "0" }}>
+          <div className={`col-10 ${styles.info}`}>
+            <div className="row">
+              <div className={`col-lg-3 ${styles.panel}`}>
+                <i className="bi bi-stopwatch-fill"></i>
+                <div className={styles.textPanel}>
+                  <h6>Cuma 15 menit</h6>
+                  <p>sudah diatas piring</p>
+                </div>
+              </div>
+              <div className={`col-lg-3 ${styles.panel}`}>
+                <i className="bi bi-patch-check"></i>
+                <div className={styles.textPanel}>
+                  <h6>Bahan segar & halal</h6>
+                  <p>jaminan mutu</p>
+                </div>
+              </div>
+              <div className={`col-lg-3 ${styles.panel}`}>
+                <i className="bi bi-geo-alt"></i>
+                <div className={styles.textPanel}>
+                  <h6>Tracking</h6>
+                  <p>semua pesanan</p>
+                </div>
+              </div>
+              <div className={`col-lg-3 ${styles.panel}`}>
+                <i className="bi bi-coin"></i>
+                <div className={styles.textPanel}>
+                  <h6>Free Ongkir</h6>
+                  <p>min. order IDR 70k</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
