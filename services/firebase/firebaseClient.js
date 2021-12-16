18998546e36878ed4cf4802fb15a88b0efc8a698
ansfdli <83810066+ansfdli@users.noreply.github.com>
2021-12-16 18:45:17 +0700

    Update firebaseClient.js

--- services/firebase/firebaseClient.js
+++ services/firebase/firebaseClient.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBkNBmjgzESRcvZUA3Y4rI2S8XJ3EqhcWI",
-  authDomain: "mendoanid.firebaseapp.com",
-  projectId: "mendoanid",
-  storageBucket: "mendoanid.appspot.com",
-  messagingSenderId: "521537107475",
-  appId: "1:521537107475:web:fce186e56e42f93f28f1e6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export default function firebaseClient() {
